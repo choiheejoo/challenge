93539b0a3ac53eb36831076acee8276827e14ff8
choiheejoo <heeju090603@gmail.com>
2023-08-18 00:41:32 +0900

    start

---
+++ src/test/java/com/modakbool/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modakbool.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
